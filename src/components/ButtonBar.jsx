88e9d25622e9e9d6d771208a3aaecd514697ee10
Eric Morrison <eric_m250@outlook.com>
2025-10-21 11:54:20 -0700

    fixed download attribute in button bar.

--- src/components/ButtonBar.jsx
+++ src/components/ButtonBar.jsx
@@ -6,7 +6,7 @@ export function ButtonBar() {
         <div className="flex gap-8 rounded-xl bg-gradient-to-br from-slate-900 via-blue-900 to-purple-900 w-max p-4 mx-auto mt-16">
             <a className="rounded-lg bg-gradient-to-r from-cyan-400 to-blue-500 text-white text-sm md:text-xl source-code-pro-regular p-2 md:px-6 md:py-3 hover:shadow-[0_0_20px_rgba(34,211,238,0.5)] transition-all duration-300 shadow-lg"
                 href={`${import.meta.env.BASE_URL}Eric_Morrison_Resume.pdf`}
-                download="Eric_Morrison_resume.pdf">
+                download="Eric_Morrison_Resume.pdf">
                 Download my CV</a>
             <button className="rounded-lg bg-gradient-to-r from-cyan-400 to-blue-500 text-white text-sm md:text-xl source-code-pro-regular p-2 md:px-6 md:py-3 hover:shadow-[0_0_20px_rgba(34,211,238,0.5)] transition-all duration-300 shadow-lg">View my LinkedIn</button>
         </div>
